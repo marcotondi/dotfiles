7395d57412d59b69bcf58a8a8f8aa33354ed1647
Marco Tondi <marcotondi@gmail.com>
2023-12-09 16:57:50 +0100

    fix: modify init.ps1 and add winget.ps1

--- init.ps1
+++ init.ps1
@@ -1,22 +1,23 @@
 ###############################################################################
 ### Init .DotFile Configurations                                              #
 ###############################################################################
-# SymbolicLink # HardLink
+# Set Variable
+$dir_dotfiles = ".\.dotfile"
 
-#INSTALL MODULE
-Install-Module -Name  posh-git -Scope CurrentUser -Force
+# INSTALL MODULE
+Install-Module -Name posh-git -Scope CurrentUser -Force
 Install-Module -Name PSFzf -Scope CurrentUser -Force
-Install-Module -Name  Terminal-Icons -Scope CurrentUser -Force
+Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
 
 # SymbolicLink # HardLink
 # PowerShell profile
-new-item -itemtype symboliclink -path "$env:USERPROFILE\Documents\PowerShell" -name Microsoft.PowerShell_profile.ps1 -value "$env:USERPROFILE\.dotfile\powershell\profile.ps1"
-new-item -itemtype symboliclink -path "$env:USERPROFILE\Documents\PowerShell" -name takuya.omp.json -value "$env:USERPROFILE\.dotfile\powershell\takuya.omp.json"
+new-item -itemtype symboliclink -path "$env:USERPROFILE\Documents\PowerShell" -name Microsoft.PowerShell_profile.ps1 -value "$env:USERPROFILE\$dir_dotfiles\powershell\profile.ps1"
+new-item -itemtype symboliclink -path "$env:USERPROFILE\Documents\PowerShell" -name takuya.omp.json -value "$env:USERPROFILE\$dir_dotfiles\powershell\takuya.omp.json"
     
 # .gitconifg
-New-Item -ItemType symboliclink -Path "$env:USERPROFILE" -Name ".gitconfig" -Value "$env:USERPROFILE\.dotfile\.gitconfig"
+New-Item -ItemType symboliclink -Path "$env:USERPROFILE" -Name ".gitconfig" -Value "$env:USERPROFILE\.$dir_dotfile\.gitconfig"
 
 # NeoVim
 mkdir $env:USERPROFILE\AppData\Local\nvim
-New-Item -ItemType symboliclink -Path "$env:USERPROFILE\AppData\Local\nvim" -Name "init.lua" -Value "$env:USERPROFILE\.dotfile\nvim\init.lua"
+New-Item -ItemType symboliclink -Path "$env:USERPROFILE\AppData\Local\nvim" -Name "init.lua" -Value "$env:USERPROFILE\$dir_dotfile\nvim\init.lua"
 
---
+++ winget.ps1
@@ -0,0 +1,2 @@
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install fzf
